67cfccf69763172693c75042cff0fe7ca71b1e66
Deepak Ugale <ugale.deepak3010@gmail.com>
2024-07-29 14:26:51 +0000

    adding the status.

---
+++ hello_deepak/src/test/java/com/spring_with_code_codespaces/hello_deepak/HelloDeepakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_with_code_codespaces.hello_deepak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDeepakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
