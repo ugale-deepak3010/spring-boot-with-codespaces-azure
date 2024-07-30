1b9a5d4a142e3be8da91048bbf58f4d9c12df80c
Deepak Ugale <ugale.deepak3010@gmail.com>
2024-07-30 08:07:59 +0000

    the data

---
+++ hello_deepak/src/main/java/com/spring_with_code_codespaces/hello_deepak/Controller/HelloDeep.java
@@ -0,0 +1,7 @@
+package com.spring_with_code_codespaces.hello_deepak.Controller;
+
+public class HelloDeep {
+
+    
+
+}
--- hello_deepak/src/main/java/com/spring_with_code_codespaces/hello_deepak/HelloDeepakApplication.java
+++ hello_deepak/src/main/java/com/spring_with_code_codespaces/hello_deepak/HelloDeepakApplication.java
@@ -3,8 +3,10 @@ package com.spring_with_code_codespaces.hello_deepak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HelloDeepakApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloDeepakApplication.class, args);
