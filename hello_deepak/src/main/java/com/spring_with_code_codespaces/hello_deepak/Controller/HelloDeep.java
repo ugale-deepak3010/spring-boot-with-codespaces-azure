36e24c13109335b352c7131715f0f2ab4450cf70
Deepak Ugale <ugale.deepak3010@gmail.com>
2024-07-30 08:17:32 +0000

    Simple controller added.

--- hello_deepak/src/main/java/com/spring_with_code_codespaces/hello_deepak/Controller/HelloDeep.java
+++ hello_deepak/src/main/java/com/spring_with_code_codespaces/hello_deepak/Controller/HelloDeep.java
@@ -1,7 +1,19 @@
 package com.spring_with_code_codespaces.hello_deepak.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/")
 public class HelloDeep {
 
+    @GetMapping
+    public String getHello() {
+        return "Hello Deepak!";
+    }
+    
     
 
 }
